7b3d8b65198b9874871cc0337f37ab29da9968f1
NellyGS01 <100099928+NellyGS01@users.noreply.github.com>
2024-03-10 19:15:11 -0600

    Mongo

--- Tarea5/pruebadatabasemongodb.py
+++ Tarea5/pruebadatabasemongodb.py
@@ -3,13 +3,17 @@ import pymongo
 from pymongo import MongoClient
 
 
-cluster = MongoClient('mongodb+srv://nellyalethia:12345@cluster0.auxtoop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-db  = cluster["prueba"]
+cluster = MongoClient('mongodb+srv://nellyalethia:12345@cluster0.auxtoop.mongodb.net/?retryWrites=true&w=majority&Prueba/test=Cluster0')
+db  = cluster["Prueba"]
 collection = db["test"]
 
 post = {"_id":0, "name": "Test1",  "age" :32}
 collection.insert_one(post)
 
-result = collection.delete_one({"_id":0})
+#result = collection.delete_one({"_id":0})
 
-results = collection.update_one({})
+#results = collection.update_one({"_id":0, "name": "Test2",})
+
+
+
+#Da error la conexion
